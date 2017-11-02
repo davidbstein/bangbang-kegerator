1129f8df5258f073b5641c13df744849ae2c0dad
David Stein <stein@davids-mbp.lan>
2017-11-02 02:04:11 -0400

    fixin things

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const fs = require('fs');
-const { Client } = require('tplink-smarthome-api');
+const tp = require('tplink-smarthome-api');
+const request = require('request')
 
-const client = new Client();
+const client = new tp.Client();
 
 const f2c = (f) => (f-32) * 5 / 9;
 
 const c2f = (c) => (9/5) * c + 32;
 
-const getTempC = () => parseInt(fs.readFileSync('/sys/bus/w1/devices/28-000008e53064/w1_slave', 'ascii').split(' ').pop().substr(2)) / 1000;
+const getTempC = () => 35 //parseInt(fs.readFileSync('/sys/bus/w1/devices/28-000008e53064/w1_slave', 'ascii').split(' ').pop().substr(2)) / 1000;
 
-const getTempF = () => c2f(getTempF)
+const getTempF = () => c2f(getTempC())
 
 const min_temp = 80;
 const max_temp = 90;
 
 const log = (msg) => {
-	fetch(new Request(
-			'https://script.google.com/macros/s/AKfycbyOuqPq-beokencETmDUHs7Vmcc1PUQ7ERQD09PPvVMevJcuxw/dev', 
-			{method: 'POST', body: 'andanother'}
-	));
+	console.log(msg);
+	request.post(
+		'https://script.google.com/macros/s/AKfycbyOuqPq-beokencETmDUHs7Vmcc1PUQ7ERQD09PPvVMevJcuxw/dev', 
+		{body: msg}
+	);
 }
 
 const setBangBang = (device) => {
 	const curTemp = getTempF();
 	if (curTemp < min_temp) {
-		log("device on! temp: " + curTemp);
-		device.setPowerState(true);
-	} else if (curTemp > max_temp)
 		log("device off! temp: " + curTemp);
 		device.setPowerState(false);
+	} else if (curTemp > max_temp) {
+		log("device on! temp: " + curTemp);
+		device.setPowerState(true);
 	} else {
 		log("nothing! temp: " + curTemp);
 	}
